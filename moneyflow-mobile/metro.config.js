5365926df1ec3564f3c9450b34c144b418ca6bfc
jay <wjay.inclino@gmail.com>
2025-07-14 15:32:28 +0800

    update metro config

--- moneyflow-mobile/metro.config.js
+++ moneyflow-mobile/metro.config.js
@@ -1,17 +1,15 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = {
-  resolver: {
-    alias: {
-      '@': './src',
-      '@components': './src/components',
-      '@screens': './src/screens',
-      '@services': './src/services',
-      '@utils': './src/utils',
-      '@types': './src/types',
-      '@store': './src/store',
-    },
-  },
+const config = getDefaultConfig(__dirname);
+
+config.resolver.alias = {
+  '@': './src',
+  '@components': './src/components',
+  '@screens': './src/screens',
+  '@services': './src/services',
+  '@utils': './src/utils',
+  '@types': './src/types',
+  '@store': './src/store',
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = config;
